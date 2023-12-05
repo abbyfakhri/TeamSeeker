df439eacfebb5732cf65162f56d2fc00fb8156a0
abbyfakhri <abbyfakhrichoiry@gmail.com>
2023-12-05 22:34:25 +0700

    bind to global ip

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(BASE_PATH, routes)
 
 function serverConn(){
-     app.listen(PORT, () =>{
-          console.log(`Server is running on http://localhost:${PORT}`)
+     app.listen(PORT,'0.0.0.0', () =>{
+          console.log(`Server is running on port: ${PORT}`)
      })
 }
 
